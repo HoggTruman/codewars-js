92d8842e969df216e39948524d709a3551f03fb0
Truman Hogg <hoggtruman@gmail.com>
2025-04-15 20:01:52 +0100

    Update RecursiveReplication

--- 7 kyu/recursive-replication.js
+++ 7 kyu/recursive-replication.js
@@ -2,10 +2,6 @@ function replicate(times, number) {
     if (times < 1) {
         return [];
     }
-    
-    if (times === 1) {
-        return [number];
-    }
 
     return replicate(times - 1, number).concat(number);    
-  }
+}
