d53174cb1a0306347bad4c5cda991fc89d061754
Truman Hogg <hoggtruman@gmail.com>
2026-01-22 21:47:41 +0000

    Add toggle-set-and-clear-bits.js

---
+++ 7 kyu/toggle-set-and-clear-bits.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/696eacb39271f8aa43b61841
+
+// 1. Toggle a bit at a given position
+const toggleBit = (n, position) => {
+    const mask = 1 << position;
+    return n ^ mask;
+};
+
+// 2. Set a bit at a given position
+const setBit = (n, position) => {
+    const mask = 1 << position;
+    return n | mask;
+};
+
+// 3. Clear a bit at a given position
+const clearBit = (n, position) => {
+    const mask = 1 << position;
+    return n & ~mask;
+};
+
+// 4. Check if a bit at a given position is set
+const isBitSet = (n, position) => {
+    const mask = 1 << position;
+    return (n & mask) != 0
+};
+
+// 5. Set multiple bits using a mask
+const setMultipleBits = (n, mask) => {
+    return n | mask;
+};
+
+// 6. Clear multiple bits using a mask
+const clearMultipleBits = (n, mask) => {
+    return n & ~mask;
+};
+
+// 7. Toggle multiple bits using a mask
+const toggleMultipleBits = (n, mask) => {
+    return n ^ mask;
+};
